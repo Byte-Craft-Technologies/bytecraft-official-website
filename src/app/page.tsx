2912a46cf1295fbcff4d8b96b55946c59cd6c810
Oluwatoyin D. FALY <46930972+oluwatoyinfaly@users.noreply.github.com>
2024-06-29 13:29:33 +0100

    Add dynamic navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import NavBar from "../app/ui/Navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-end justify-between p-10 bg-[#F5F5F5]">
-      <NavBar />
+    <main className="flex min-h-screen flex-col items-end justify-between p-10 bg-[#F5F5F5]">      <NavBar />
       <div className="flex flex-col items-center justify-center space-y-8">
       </div>
     </main>
--- src/app/ui/Navbar/index.tsx
+++ src/app/ui/Navbar/index.tsx
@@ -1,34 +1,75 @@
+'use client'
+
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { inter } from '../fonts';
 import Button from '../../components/button';
 import { ChevronDown } from '../icons';
 
+const links = [
+  { name: 'Accueil', href: '#' },
+  { name: 'Services', href: '#', submenu: [{ name: 'Service 1', href: '#' }, { name: 'Service 2', href: '#' }] },
+  { name: 'Réalisations', href: '#' },
+];
 
 export default function NavBar() {
-    return (
-        <header className={`${inter.className} flex items-baseline text-black`}>
-            <nav className="pt-2 pb-6 grid grid-cols-3 mx-24 justify-around">
-                <div className='w-full'>
-                    <Link href="#">Accueil</Link>
+  const [submenuVisibility, setSubmenuVisibility] = useState<{ [key: string]: boolean }>({});
+  const [selectedLink, setSelectedLink] = useState<string | null>(null);
+
+  const toggleSubmenu = (name: string) => {
+    setSubmenuVisibility((prev) => ({ ...prev, [name]: !prev[name] }));
+  };
+
+  const handleLinkClick = (name: string) => {
+    setSelectedLink(name);
+    if (links.find(link => link.name === name)?.submenu) {
+      return false;
+    }
+  };
+
+  return (
+    <header className={`${inter.className} flex items-center justify-around text-black w-full shadow-sm shadow-slate-200`}>
+      <div className='pt-2 pb-6 items-start'>Logo</div>
+      <div className='flex items-baseline'>
+        <nav className="pt-2 pb-6 grid grid-cols-3 mx-24 justify-around">
+          {links.map((link) => (
+            <div key={link.name} className={`w-full pr-8 ${selectedLink === link.name ? 'text-black' : 'text-[#8987A1]'}`}>
+              {link.submenu ? (
+                <div className='text-[#8987A1] pr-6'>
+                  <Link href="#" onClick={() => { toggleSubmenu(link.name); handleLinkClick(link.name); }} className='flex flex-wrap'>
+                    {link.name}
+                    <ChevronDown fill="#8987A1" size={16} height={16} width={16} className="mx-1 mt-1.5" />
+                  </Link>
+                  {submenuVisibility[link.name] && (
+                    <div className="flex flex-col absolute bg-white shadow-slate-200 shadow-xl mt-1">
+                      {link.submenu.map((subItem) => (
+                        <div key={subItem.name} >
+                          <div className='my-2 w-auto mx-5'>
+                            <Link href={subItem.href} onClick={() => handleLinkClick(subItem.name)}>{subItem.name}</Link>
+                          </div>
+                          <hr className="h-px bg-gray-200 border-0 dark:bg-gray-400"></hr>
+                        </div>
+                      ))}
+                    </div>
+                  )}
                 </div>
-                <div className='w-full text-[#8987A1] flex flex-wrap'>
-                    <Link href="#">Services</Link>
-                    <ChevronDown fill="#8987A1" size={16} height={16} width={16} className="" />
-                </div>
-                <div className='w-full text-[#8987A1]'>
-                    <Link href="#">Réalisations</Link>
-                </div>
-            </nav>
-            <div className='px-2'>
-                <Button
-                    name="Devis"
-                    link="#"
-                    borderStyle="rounded-xl px-8 py-2"
-                    fontStyle="font-se text-base"
-                    bgColor="bg-primary"
-                    textColor="text-white"
-                />
+              ) : (
+                <Link href={link.href} onClick={() => handleLinkClick(link.name)}>{link.name}</Link>
+              )}
             </div>
-        </header>
-    );
+          ))}
+        </nav>
+        <div className='px-1 pt-0'>
+          <Button
+            name="Devis"
+            link="#"
+            borderStyle="rounded-xl px-8"
+            fontStyle="font-se text-base"
+            bgColor="bg-primary"
+            textColor="text-white"
+          />
+        </div>
+      </div>
+    </header>
+  );
 }
