42272c46a878a018d296dc3c9f624b7b55edbfe0
Oluwatoyin D. FALY <46930972+oluwatoyinfaly@users.noreply.github.com>
2024-07-04 21:22:44 +0100

    display card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import NavBar from "../app/ui/Navbar";
+import Card from "./components/card";
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-end justify-between p-10 bg-[#F5F5F5]">      <NavBar />
+    <main className="flex min-h-screen flex-col justify-between p-10 bg-[#F5F5F5]">
+      <NavBar />
+      <Card
+        title="Prêt à propulser votre Business vers de nouveaux sommets de croissance ?"
+        description="Boostez votre entreprise avec notre expertise. Contactez-nous dès aujourd'hui !"
+        image="/hero-image-1.png"
+        link="/"
+        buttonText="Contactez-nous"
+      />
       <div className="flex flex-col items-center justify-center space-y-8">
       </div>
     </main>
